6bf182f04213e1a0e524a7d52df406ef65ec889a
Peter Oshaughnessy <peter.oshaughnessy@gmail.com>
2018-10-30 15:29:17 +0000

    More work on llama vision

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ A web page that detects llamas through your camera.
 
 Load it up and point your camera at a llama. If you don't have a real llama to hand (sad 😦), you can try pointing it at a picture or video of a llama.
 
-*Please note:* This is a _Llama_ Detector. Trying to fool the Dector with an alpaca is plain cheating and cheating is wrong. Go take a long, hard look at yourself. Yes, you. We know your type. 😒
+**Please note:** This is a _Llama_ Detector. Trying to fool the Dector with an alpaca, or another non-llama [camelid](https://en.wikipedia.org/wiki/Camelid), is cheating - and cheating is wrong. Go take a long, hard look at yourself. Yes, you. We know your type. 😒
 
 ## Development
 
--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,12 @@
 
   const DETECTION_INTERVAL_MILLIS = 2000;
 
-  const video = document.getElementById('video');
-  const pageIntro = document.getElementById('page-intro');
-  const pageDetector = document.getElementById('page-detector');
-  const detectedMessage = document.getElementById('detected');
+  const video = document.querySelector('video');
+  const pages = document.querySelectorAll('.page');
+  const supportedDiv = document.getElementById('supported');
+  const unsupportedDiv = document.getElementById('unsupported');
+  const errorMsg = document.getElementById('error-msg');
+  let detecting = false;
 
   let predictionModel;
 
@@ -13,26 +15,27 @@
 
     setInterval(() => {
 
-      console.log('TEST 3');
-
       predictionModel.classify(video).then(predictions => {
 
-        console.log('TEST 4');
-
+        if (!detecting) {
+          document.body.classList.add('detecting');
+          detecting = true;
+        }
+        
         const topResult = predictions[0];
   
         if (topResult.className === 'llama') {
           console.log('OMG llama!', topResult);
-          detectedMessage.style.display = 'block';
+          document.body.classList.add('detected');
         } else {
           console.log('No llama...', predictions);
-          detectedMessage.style.display = 'none';
+          document.body.classList.remove('detected');
         }
     
       })
       .catch(err => {
         console.error('classify error', err);
-        // TODO showUnsupported();
+        showUnsupported(err);
       });;
 
     }, DETECTION_INTERVAL_MILLIS);
@@ -71,40 +74,56 @@
           video.src = window.URL.createObjectURL(stream);
         }
 
-        console.log('TEST 1');
-
         mobilenet.load().then(model => {
           predictionModel = model;
-          console.log('TEST 2');
           detectLlamas();
         });
 
       })
       .catch(err => {
         console.error('getUserMedia error', err);
-        // TODO showUnsupported();
+        showUnsupported(err);
       });
 
+  }
+
+  function showSupported() {
+    showPage('intro');
+    supportedDiv.style.display = 'block';
+    unsupportedDiv.style.display = 'none';
+  }
+
+  function showUnsupported(error) {
+    errorMsg.innerHTML = error;
+    showPage('intro');
+    unsupportedDiv.style.display = 'block';
+    supportedDiv.style.display = 'none';
+  }
+
+  function showPage(pageName) {
+    pages.forEach(page => {
+      page.classList.add('hidden');
+    });
+    const pageEl = document.getElementById(`page-${pageName}`);
+    pageEl.classList.remove('hidden');
   }
 
   function init() {
 
       if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-        // TODO showUnsupported();
+        showUnsupported();
         return;
       }
-      
+
       const btnGo = document.getElementById('btn-go');
 
       btnGo.addEventListener('click', () => {
-
-        pageIntro.style.display = 'none';
-        pageDetector.style.display = 'block';
-
         setupCamera();
-
+        showPage('detector');
       });
 
+      showSupported();
+      
   }
 
   init();
