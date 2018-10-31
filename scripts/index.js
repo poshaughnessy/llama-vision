97f2ec69422ac43005d748dbf5c4ac84f5ea9fae
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2018-10-31 13:47:35 +0000

    Fix short name, improve contrast, add service worker

--- scripts/index.js
+++ scripts/index.js
@@ -131,6 +131,20 @@
 
     showSupported();
 
+    if ('serviceWorker' in navigator) {
+
+      navigator.serviceWorker.register('/scripts/service-worker.js')
+        .then(() => {
+          console.log('Service worker successfully registered');
+        })
+        .catch(err => {
+          console.error('Service worker failed to register', err);
+        });
+
+    } else {
+      console.log('Service workers not supported');
+    }
+
   }
 
   init();
