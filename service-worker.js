8658d7d2660696f11bf1a675a6390fa677952905
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2018-10-31 13:57:40 +0000

    Update service worker, defer scripts

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,24 @@
-// Based on the "Offline copy of pages" service worker
-// From: https://www.pwabuilder.com
+// Partly based on "Offline copy of pages" service worker from: https://www.pwabuilder.com
+
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/styles/styles.css',
+  '/images/llama-lens-right.svg'
+];
 
 // Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener('install', event => {
-  const indexPage = new Request('index.html');
-  event.waitUntil(
-    fetch(indexPage).then(response => {
-      return caches.open('pwabuilder-offline').then(cache => {
-        console.log('[PWA Builder] Cached index page during Install' + response.url);
-        return cache.put(indexPage, response);
+
+  function onInstall() {
+    return caches.open('pwabuilder-offline')
+      .then(cache => {
+        console.log('Caching pre-defined assets on installation');
+        return cache.addAll(PRECACHE_URLS);
       });
-    }));
+  }
+
+  event.waitUntil(onInstall(event));
 });
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
@@ -18,7 +26,7 @@ self.addEventListener('fetch', event => {
   const updateCache = request => {
     return caches.open('pwabuilder-offline').then(cache => {
       return fetch(request).then(response => {
-        console.log('[PWA Builder] add page to offline' + response.url)
+        console.log('[PWA Builder] add page to offline' + response.url);
         return cache.put(request, response);
       });
     });
